f2c92d6b228447a1d9970bb325667c2e88c17aba
AriMC <anacast.432@gmail.com>
2024-10-10 01:28:39 -0600

    Rectangulo

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+//Ejemplo de trazo
+/*ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100);*/
+
+// 1.-Ejemplo de trazo de Rectangulo
+
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+
