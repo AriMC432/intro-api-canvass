ad29145c57dd6830e19531e193e15998f2aafcfc
AriMC <anacast.432@gmail.com>
2024-10-10 01:46:24 -0600

    Trinagulo

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ ctx.fillRect(10, 10, 100, 100);*/
 
 // 1.-Ejemplo de trazo de Rectangulo
 
-      ctx.fillRect(25, 25, 100, 100);
-      ctx.clearRect(45, 45, 60, 60);
-      ctx.strokeRect(50, 50, 50, 50);
+    //ctx.fillRect(25, 25, 100, 100);
+    //ctx.clearRect(45, 45, 60, 60);
+    //ctx.strokeRect(50, 50, 50, 50);
+
+// 2.- Ejemplo de trazo de Triangulo
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
+
 
