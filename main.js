b9d60a5353b316b19821125e4c66f7ca11bea1e6
AriMC <anacast.432@gmail.com>
2024-10-10 01:56:17 -0600

    Pluma

--- main.js
+++ main.js
@@ -18,4 +18,15 @@ ctx.fillRect(10, 10, 100, 100);*/
     ctx.lineTo(100, 25);
     ctx.fill();
 
+// 3.- Ejemplo de trazo de pluma
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Círculo externo
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false); // Boca (en el sentido de las agujas del reloj)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Ojo izquierdo
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Ojo derecho
+    ctx.stroke();
+
 
